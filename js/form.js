bc2b13011e80fdc29be55d135c2f88dd71e94795
Bhagyashree <bhagya099@gmail.com>
2021-08-18 16:00:26 +1000

    complete the modal popup using js

--- js/form.js
+++ js/form.js
@@ -2,6 +2,8 @@
 
 const lastName = document.querySelector("#last_name");
 const form = document.querySelector("#contact-form");
+const btnCloseModal = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
 
 // regexr
 // /^[A-Za-z0-9\._\-]+@[A-Za-z]+[A-Za-z0-9\-]+[A-Za-z]+(\.[A-Za-z0-9\-]+)+
@@ -18,8 +20,20 @@ form.onsubmit = submitForm;
 function submitForm(e) {
     e.preventDefault();
     console.log("hello");
+    openModal();
 }
 
 function validInput(regex, input) {
     return regex.test(input.value);
 }
+
+// for opening modal
+const openModal = function() {
+    modal.classList.remove("hidden");
+};
+// for closing modal
+const closeModal = function() {
+    modal.classList.add("hidden");
+};
+// use close function closing button
+btnCloseModal.addEventListener("click", closeModal);
